7ccc1ce28babcdcaf30d017b711f15c33b620ad8
hitz patel <destiny.creator1959@gmail.com>
2023-10-21 23:22:32 +0100

    sunday22

---
+++ src/main/java/q1.java
@@ -0,0 +1,9 @@
+public class q1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Name- Hitesh Patel");
+        System.out.println("Hello world");
+        System.out.println("The Earth is beautiful");
+
+    }
+}
---
+++ src/main/java/q2.java
@@ -0,0 +1,8 @@
+public class q2 {
+    public static void main (String[]args) {
+        int a = 74;
+        int b = 36;
+
+        System.out.println(a + b);
+    }
+}
