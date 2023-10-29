394d3ec03ab7491f1c7db9a80ba35f847033aa6b
hites <destiny.creator1959@gmail.com>
2023-10-29 19:41:37 +0000

    week7

---
+++ src/main/week6/q18.java
@@ -0,0 +1,18 @@
+public class q18 {
+
+    public static void main(String[] args)
+    {
+        System.out.println("============================");
+        System.out.println("|     @  MARK SHEET @       |");
+        System.out.println("| [SUBJECT]::::::::::[MARKS]|");
+        System.out.println("|   MATHS               95  |");
+        System.out.println("|   SCIENCE             97  |");
+        System.out.println("|   SANSKRIT            89  |");
+        System.out.println("|   GUJARATI            74  |");
+        System.out.println("|   SOCIAL STUDY        86  |");
+        System.out.println("|   ENGLISH             78  |");
+        System.out.println("=============================");
+
+    }
+}
+
